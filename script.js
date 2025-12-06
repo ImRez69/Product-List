58183d23c13fd291ac24068462978f85f5e88c9a
ImRez69 <CaptianAlirez@gmail.com>
2025-12-06 21:49:34 +0330

    Add Laod More

--- script.js
+++ script.js
@@ -1,31 +1,407 @@
 const main = document.querySelector("main");
+const loadMore = document.getElementById("load-more");
+let loadTimes = 0;
 const productList = [
   {
     id: 1,
     title:
       "گوشی شیائومی Redmi Note 14 4G رجیستر‌شده دو سیم‌کارت 256 گیگابایت با رم 8 گیگابایت",
-    price: "19399000", // ۱۹,۳۹۹,۰۰۰
+    price: "19399000",
     image:
-      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90",
-    link: "https://www.digikala.com/product/dkp-17580036/%DA%AF%D9%88%D8%B4%DB%8C-%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84-%D8%B4%DB%8C%D8%A7%D8%A6%D9%88%D9%85%DB%8C-%D9%85%D8%AF%D9%84-redmi-note-14-4g-%D8%AF%D9%88-%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA-%D8%B8%D8%B1%D9%81%DB%8C%D8%AA-256-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%D9%88-%D8%B1%D9%85-8-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA/?bCode=204481",
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
   },
   {
     id: 2,
-    title:
-      "گوشی شیائومی Redmi Note 14 4G رجیستر‌شده دو سیم‌کارت 256 گیگابایت با رم 8 گیگابایت",
-    price: "43450000", // ۴۳,۴۵۰,۰۰۰
+    title: "گوشی موبایل مدل تست 2 دو سیم‌کارت 128 گیگابایت با رم 6 گیگابایت",
+    price: "10500000",
     image:
-      "https://dkstatics-public.digikala.com/digikala-products/6144c3fdde10110e4312420db31e07f42e2cfc3a_1738146333.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90",
-    link: "https://www.digikala.com/product/dkp-17986248/%DA%AF%D9%88%D8%B4%DB%8C-%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84-%D8%B4%DB%8C%D8%A7%D8%A6%D9%88%D9%85%DB%8C-%D9%85%D8%AF%D9%84-poco-x7-pro-%D8%AF%D9%88-%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA-%D8%B8%D8%B1%D9%81%DB%8C%D8%AA-512-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%D9%88-%D8%B1%D9%85-12-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%DA%AF%D9%84%D9%88%D8%A8%D8%A7%D9%84/?bCode=204481",
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
   },
   {
     id: 3,
-    title:
-      "گوشی شیائومی Redmi Note 14 4G رجیستر‌شده دو سیم‌کارت 256 گیگابایت با رم 8 گیگابایت",
-    price: "60254800", // ۶۰,۲۵۴,۸۰۰
+    title: "گوشی موبایل مدل تست 3 تک سیم‌کارت 256 گیگابایت با رم 12 گیگابایت",
+    price: "15200000",
     image:
-      "https://dkstatics-public.digikala.com/digikala-products/915fa5f904618d5c06f10fad876e8666a6e5c000_1737474361.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90",
-    link: "https://www.digikala.com/product/dkp-16736269/%DA%AF%D9%88%D8%B4%DB%8C-%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84-%D8%B3%D8%A7%D9%85%D8%B3%D9%88%D9%86%DA%AF-%D9%85%D8%AF%D9%84-s24-fe-%D8%AF%D9%88-%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA-%D8%B8%D8%B1%D9%81%DB%8C%D8%AA-256-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%D9%88-%D8%B1%D9%85-8-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%D9%88%DB%8C%D8%AA%D9%86%D8%A7%D9%85/?bCode=204481",
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 4,
+    title: "گوشی موبایل مدل تست 4 دو سیم‌کارت 64 گیگابایت با رم 4 گیگابایت",
+    price: "7200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 5,
+    title: "گوشی موبایل تست 5 نسخه 5G ظرفیت 256 گیگابایت رم 8",
+    price: "18990000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 6,
+    title: "گوشی موبایل تست 6 نسخه پلاس 128 گیگابایت رم 8",
+    price: "13400000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 7,
+    title: "گوشی موبایل تست 7 اولترا با ظرفیت 512 گیگابایت و رم 12 گیگابایت",
+    price: "28990000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 8,
+    title: "گوشی موبایل تست 8 نسخه معمولی 64 گیگابایت رم 4",
+    price: "6500000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 9,
+    title: "گوشی موبایل تست 9 نسخه اقتصادی 32 گیگابایت رم 3",
+    price: "4800000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 10,
+    title: "گوشی موبایل تست 10 نسخه پرو 128 گیگابایت رم 8",
+    price: "15900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 11,
+    title: "گوشی موبایل تست 11 دو سیم‌کارت 256 گیگابایت رم 12",
+    price: "21400000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 12,
+    title: "گوشی موبایل تست 12 نسخه استاندارد 128 گیگابایت رم 6",
+    price: "11200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 13,
+    title: "گوشی موبایل تست 13 اولترا 512 گیگابایت رم 16",
+    price: "34900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 14,
+    title: "گوشی موبایل تست 14 اقتصادی 64 گیگابایت رم 4",
+    price: "6800000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 15,
+    title: "گوشی موبایل تست 15 پرو 256 گیگابایت رم 12",
+    price: "22900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 16,
+    title: "گوشی موبایل تست 16 تک سیم‌کارت 128 گیگابایت رم 4",
+    price: "8900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 17,
+    title: "گوشی موبایل تست 17 نسخه S ظرفیت 256 گیگابایت رم 8",
+    price: "17500000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 18,
+    title: "گوشی موبایل تست 18 نسخه M ظرفیت 128 گیگابایت رم 6",
+    price: "10200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 19,
+    title: "گوشی موبایل تست 19 نسخه X ظرفیت 512 گیگابایت رم 16",
+    price: "31200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 20,
+    title: "گوشی موبایل تست 20 اقتصادی 32 گیگابایت رم 2",
+    price: "3900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 21,
+    title: "گوشی تست 21",
+    price: "7200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 22,
+    title: "گوشی تست 22",
+    price: "8300000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 23,
+    title: "گوشی تست 23",
+    price: "9100000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 24,
+    title: "گوشی تست 24",
+    price: "6000000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 25,
+    title: "گوشی تست 25",
+    price: "15900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 26,
+    title: "گوشی تست 26",
+    price: "13200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 27,
+    title: "گوشی تست 27",
+    price: "4900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 28,
+    title: "گوشی تست 28",
+    price: "7600000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 29,
+    title: "گوشی تست 29",
+    price: "9200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 30,
+    title: "گوشی تست 30",
+    price: "11000000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 31,
+    title: "گوشی تست 31",
+    price: "5100000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 32,
+    title: "گوشی تست 32",
+    price: "6800000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 33,
+    title: "گوشی تست 33",
+    price: "8700000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 34,
+    title: "گوشی تست 34",
+    price: "9500000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 35,
+    title: "گوشی تست 35",
+    price: "7200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 36,
+    title: "گوشی تست 36",
+    price: "13900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 37,
+    title: "گوشی تست 37",
+    price: "23000000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 38,
+    title: "گوشی تست 38",
+    price: "4100000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 39,
+    title: "گوشی تست 39",
+    price: "9800000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 40,
+    title: "گوشی تست 40",
+    price: "11400000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 41,
+    title: "گوشی تست 41",
+    price: "5600000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 42,
+    title: "گوشی تست 42",
+    price: "12900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 43,
+    title: "گوشی تست 43",
+    price: "20600000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 44,
+    title: "گوشی تست 44",
+    price: "3900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 45,
+    title: "گوشی تست 45",
+    price: "5300000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 46,
+    title: "گوشی تست 46",
+    price: "7800000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 47,
+    title: "گوشی تست 47",
+    price: "14200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 48,
+    title: "گوشی تست 48",
+    price: "26700000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 49,
+    title: "گوشی تست 49",
+    price: "9900000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
+  },
+  {
+    id: 50,
+    title: "گوشی تست 50",
+    price: "12000000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/c0a7ce006d11c222bce16bd3cdd8d79aeb6689bc_1738134271.jpg",
+    link: "https://www.digikala.com/product/dkp-17580036",
   },
 ];
 
@@ -70,6 +446,22 @@ function addToMain(element) {
   main.appendChild(element);
 }
 
-productList.map((product) => {
-  createDiv(product);
+function productLoad5(itemList) {
+  const itemList5 = itemList.slice(loadTimes, loadTimes + 5);
+  itemList5.map((item) => {
+    createDiv(item);
+  });
+  loadTimes += 5;
+}
+
+function productUnLoad() {
+  main.innerHTML = "";
+  console.log(main.innerHTML);
+}
+
+productLoad5(productList);
+
+loadMore.addEventListener("click", () => {
+  productUnLoad();
+  productLoad5(productList);
 });
